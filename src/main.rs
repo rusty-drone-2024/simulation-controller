628731790b78d10ba3e2d9087027a21c32cb53b3
MarioS <mario.sguario@gmail.com>
2025-01-16 11:51:55 +0100

    Made SC main, minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-mod ui;
+use wg_2024::{controller::{self, DroneEvent}, network::NodeId, packet::Packet};
+use common_structs::network::{Network, TypeInfo};
+use network_initializer::{initialize_default_network,initialize_network_with_implementation};
 
 use bevy::prelude::*;
-use common_structs::network::{Network, TypeInfo};
-use wg_2024::network::NodeId;
+mod ui;
+
+
+fn main() {
+    let network = initialize_default_network();
+    let controller = RustySC { network };
+    controller.run();
+}
 
 #[allow(dead_code)]
 pub struct RustySC {
@@ -10,16 +18,22 @@ pub struct RustySC {
 }
 
 impl RustySC {
-    pub fn start(network: Network) {
-        let controller = RustySC { network };
-        controller.run();
-    }
-}
-
-impl RustySC {
     fn run(&self) {
-        println!("Simulation controller started...");
-        App::new().run();
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .run();
+    }
+    pub fn handle_drone_events(&mut self, event: DroneEvent) {
+        match event {
+            DroneEvent::PacketDropped(p) => todo!(),
+            DroneEvent::PacketSent(p) => todo!(),
+            DroneEvent::ControllerShortcut(p) => self.forward_shortcut(p),
+        }            
+    }
+    pub fn forward_shortcut(&self, packet: Packet) {
+        let node_id: NodeId = packet.routing_header.hops[packet.routing_header.hops.len() - 1];
+        let node = self.network.topology.get(&node_id).unwrap();
+        
     }
 
     pub fn remove_sender(&mut self, nghb_id: NodeId, id: NodeId) -> Result<(), ()> {
@@ -49,7 +63,7 @@ impl RustySC {
 
     pub fn add_sender(&mut self, nghb_id: NodeId, id: NodeId) -> Result<(), ()> {
         let node = self.network.topology.get(&id).unwrap();
-        let sender = self.network.simulation_channels.leaf_event_sender.clone();
+        let sender = self.network.topology.get(&nghb_id).unwrap().packet_in_channel.clone();
         match &node.type_info {
             TypeInfo::Drone(ref drone_info) => {
                 let _ = drone_info.command_send_channel.send(
