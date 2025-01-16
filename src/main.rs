a1ee1566514f08f6dfe2404829f82940ece6656b
MarioS <mario.sguario@gmail.com>
2025-01-16 11:54:26 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use wg_2024::{controller::{self, DroneEvent}, network::NodeId, packet::Packet};
 use common_structs::network::{Network, TypeInfo};
-use network_initializer::{initialize_default_network,initialize_network_with_implementation};
+use network_initializer::{initialize_default_network, initialize_network_with_implementation};
+use wg_2024::{
+    controller::{self, DroneEvent},
+    network::NodeId,
+    packet::Packet,
+};
 
 use bevy::prelude::*;
 mod ui;
-
 
 fn main() {
     let network = initialize_default_network();
@@ -19,21 +22,18 @@ pub struct RustySC {
 
 impl RustySC {
     fn run(&self) {
-        App::new()
-            .add_plugins(DefaultPlugins)
-            .run();
+        App::new().add_plugins(DefaultPlugins).run();
     }
     pub fn handle_drone_events(&mut self, event: DroneEvent) {
         match event {
             DroneEvent::PacketDropped(p) => todo!(),
             DroneEvent::PacketSent(p) => todo!(),
             DroneEvent::ControllerShortcut(p) => self.forward_shortcut(p),
-        }            
+        }
     }
     pub fn forward_shortcut(&self, packet: Packet) {
         let node_id: NodeId = packet.routing_header.hops[packet.routing_header.hops.len() - 1];
         let node = self.network.topology.get(&node_id).unwrap();
-        
     }
 
     pub fn remove_sender(&mut self, nghb_id: NodeId, id: NodeId) -> Result<(), ()> {
@@ -63,7 +63,13 @@ impl RustySC {
 
     pub fn add_sender(&mut self, nghb_id: NodeId, id: NodeId) -> Result<(), ()> {
         let node = self.network.topology.get(&id).unwrap();
-        let sender = self.network.topology.get(&nghb_id).unwrap().packet_in_channel.clone();
+        let sender = self
+            .network
+            .topology
+            .get(&nghb_id)
+            .unwrap()
+            .packet_in_channel
+            .clone();
         match &node.type_info {
             TypeInfo::Drone(ref drone_info) => {
                 let _ = drone_info.command_send_channel.send(
